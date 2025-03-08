f6c23166ade9ef78af1425c858ba66f5565afd86
nearystibb486 <nearystibb486@gmail.com>
2025-03-08 06:04:32 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int age = 25;
+    char name[] = "John Doe";
+
+    printf("Hello, my name is %s and I am %d years old.\n", name, age);
+
+    return 0;
+}
