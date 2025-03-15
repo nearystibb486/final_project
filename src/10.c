096696c1e8821f03bde7b12e3d1c241ffdbd66d0
nearystibb486 <nearystibb486@gmail.com>
2025-03-15 18:08:04 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number = 42;
+    char *string = "Hello World!";
+    printf("The number is %d\n", number);
+    printf("The string is %s\n", string);
+    return 0;
+}
