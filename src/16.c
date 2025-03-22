e189b913149e0b679dc94a518bf7483502631777
nearystibb486 <nearystibb486@gmail.com>
2025-03-22 16:53:55 +0000

    Add 16.c

---
+++ src/16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf("Enter number of rows: ");
+    scanf("%d", &i);
+
+    if (i > 0) {
+        printf("\n");
+    }
+
+    for (i = 1; i <= i; ++i) {
+        printf("* ");
+    }
+
+    return 0;
+}
