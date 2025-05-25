e2d1b629b9fb6b6dde3d8b28ba6a044664369a9a
nearystibb486 <nearystibb486@gmail.com>
2025-05-25 12:26:50 +0000

    Add 65.c

---
+++ src/65.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Example loop that prints numbers 1 to 5
+    for(i = 1; i <= 5; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
